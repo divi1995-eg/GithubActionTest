04703e1cc1d2233e1345933ebe008075f77b0734
divi1995-eg <divysharma@expediagroup.com>
2022-04-24 17:38:12 +0530

    Initial Commit

---
+++ GithubActionTest/GithubActionTest/GithubActionTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionTestApp.swift
+//  GithubActionTest
+//
+//  Created by Divyanshu Sharma on 24/04/22.
+//
+
+import SwiftUI
+
+@main
+struct GithubActionTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
